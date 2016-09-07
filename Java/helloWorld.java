0e0a4c6c83f639eab28e21ba4896cd10daff4c09
wcam <campbell.programming@gmail.com>
2016-09-07 12:32:41 -0500

    Update helloWorld.java

--- Java/helloWorld.java
+++ Java/helloWorld.java
@@ -1,5 +1,7 @@
-class HelloWorldApp {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+class HelloWorldApp 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World!");
     }
 }
