ca95c41b439fc1d75dfe6c096f4daf1d6808d27e
wcam <campbell.programming@gmail.com>
2016-11-22 23:39:49 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,50 +1,26 @@
-public class Player {  //this whole class is pretty self explanatory
-	int roundBalance = 0;
-	int gameBalance = 0;
-	int winCount = 0;
-	int playerCount = 0;
-	String name = "Unassigned";
-	
-	public void setName(String n){
-		this.name = n;
-	}	
-	public void setWinCount(int wc){
-		this.winCount += wc;
-	}
-	
-	public String getName(){
-		return name;
-	}
-	
-	public void setPlayerNum(int pc){
-		this.playerCount = pc;
-	}
-	
-	public int getPlayerNum(){
-		return playerCount;
-	}
-	
-	public void setRoundBalance(int rb){
-		this.roundBalance = rb;
-	}
-	
-	public int getRoundBalance(){
-		return roundBalance;
-	}
+package wheeloffortune;
+import java.util.Scanner;
 
-	public int getGameBalance(){
-		return gameBalance;
-	}
-	
-	public void setGameBalance(int gb){
-		this.gameBalance = gb;
-	}
-	
-	public void addGameBalance(int gb){
-		this.gameBalance += gb;
-	}
-	
-	public void addRoundBalance(int rb){
-		this.roundBalance += rb;
-	}
+public class Player
+{
+    public String name;
+    public int cashBalance;
+    public int roundBalance;
+    
+    public void spinWheel()
+    {
+        
+    }
+    public void solvePuzzle(String guess)
+    {
+        
+    }
+    
+    public void spinOrSolve(Puzzle p)
+    {
+        // print the puzzle as it currently is
+        // get response
+        // spinWheel() if 1
+        // solvePuzzle(String guess) if 2
+    }
 }
