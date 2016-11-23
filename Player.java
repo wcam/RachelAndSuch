e9424939f40e7cd9cd1ea75ed30991fd66540517
wcam <campbell.programming@gmail.com>
2016-11-23 13:47:37 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,24 +3,55 @@ import java.util.Scanner;
 
 public class Player
 {
-    public String name;
-    public int cashBalance;
-    public int roundBalance;
-    
-    public void spinWheel()
-    {
-        
-    }
-    public void solvePuzzle(String guess)
-    {
-        
-    }
-    
-    public void spinOrSolve(Puzzle p)
-    {
-        // print the puzzle as it currently is
-        // get response
-        // spinWheel() if 1
-        // solvePuzzle(String guess) if 2
-    }
+   public String name;
+   public int cashBalance;
+   public int roundBalance;
+   
+   public void spinWheel()
+   {
+       
+   }
+   public void solvePuzzle(String guess, Puzzle p)
+   {
+       if(guess == p.phrase)
+       {
+       	p.solved = true;
+       }
+   }
+   
+   public void spinOrSolve(Puzzle p)
+   {
+   	int spinSolve;
+   	String guess;
+   	
+   	Scanner scanner = new Scanner(System.in);
+   	// print the puzzle as it currently is
+   	System.out.println(p.phrase);
+       // get response
+   	System.out.println("Would you like to spin [1] or solve [2]?");
+   	spinSolve = scanner.nextInt();
+       // spinWheel() if 1
+   	if(spinSolve == 1)
+   	{
+   	spinWheel();
+   	}       
+   	// solvePuzzle(String guess) if 2
+   	else if(spinSolve == 2)
+   	{
+   	System.out.println("Please enter your guess: ");
+   	guess = scanner.next();
+   	solvePuzzle(guess, p);
+   	}
+   	else
+   	{
+   	System.out.println("Invalid selection! Please enter either 1 or 2");
+   	spinOrSolve(p);
+   	}
+
+   	
+   }
+   public void setName(String n)
+   {
+   	this.name = n;
+   }
 }
