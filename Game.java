d956a9fb6f1c39b03c7b8bf963d74f9b9850e5cd
wcam <campbell.programming@gmail.com>
2016-11-26 02:04:35 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -27,10 +27,91 @@ public class Game
    
    public void beginGame()
    {
+       printPuzzleData();
+       printPlayers();
+
        for (int i = 0; i < numPuzzlesToSolve; i++)
        {
-           // TODO:  start with player 0, and continue until the puzzle is solved
+           Puzzle p = getPuzzleToSolve();
+           
+           for (int j = 0; j < players.length; j++)
+           {
+               if (!p.solved)
+               {
+                   solvePuzzle(p, players[j]);
+               }
+           }
        }
+   }
+   
+   public void solvePuzzle(Puzzle puzzle, Player player)
+   {
+       System.out.println("Asking player (" + player.name + ") to spin or solve the puzzle: " + puzzle.toString());
+       player.spinOrSolve(puzzle);
+   }
+   
+   public Puzzle getPuzzleToSolve()
+   {
+       int randomCategory = getRandomCategory();
+       Puzzle p = getRandomPuzzle(randomCategory);
+       
+       System.out.println(p.toString());
+       return p;
+   }
+   
+   public Puzzle getRandomPuzzle(int category)
+   {
+       int randomPuzzleNumber = 0;
+       
+       switch(category)
+       {
+        case 1: 
+            randomPuzzleNumber = Tester.randomNumber(0, 5);
+            break;
+        case 2: 
+            randomPuzzleNumber = Tester.randomNumber(6, 10);
+            break;
+        case 3: 
+            randomPuzzleNumber = Tester.randomNumber(11, 15);
+            break;
+        case 4: 
+            randomPuzzleNumber = Tester.randomNumber(16, 20);
+            break;
+        case 5: 
+            randomPuzzleNumber = Tester.randomNumber(21, 25);
+            break;
+       }
+       
+       return puzzles[randomPuzzleNumber];
+   }
+   
+   public int getRandomCategory()
+   {
+       int categoryNumber = Tester.randomNumber(5);
+       System.out.println("Random category for this game: " + categoryNumber);
+       
+       return categoryNumber;
+   }
+   public void printPuzzleData()
+   {
+       System.out.println(" ");
+       System.out.println("Puzzles loaded from file: ");
+       for (int i = 0; i < puzzles.length; i++)
+       {
+           System.out.println("Phrase " + i + ": " + puzzles[i].phrase + ", Category: " + puzzles[i].category);
+       }
+       System.out.println(" ");
+   }
+   
+   public void printPlayers()
+   {
+       System.out.println(" ");
+       System.out.println("Players: ");
+       for (int i = 0; i < players.length; i++)
+       {
+           System.out.println(players[i].name);
+       }
+       System.out.println(" ");
    }
    
    public void solveThePuzzle(Puzzle p)
