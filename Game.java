0045359dc69841dfbbb64405b394fafa805a4d5a
wcam <campbell.programming@gmail.com>
2016-11-26 00:35:23 -0600

    Update Game.java

--- Game.java
+++ Game.java
@@ -5,25 +5,31 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Game 
 {
    public Player[] players;
    public Puzzle[] puzzles;
+   public ArrayList<Integer> playedPuzzles;
    public Game() { }
+   public boolean done;
+   public int numPuzzlesToSolve;
    
    public Game(Player[] players, Puzzle[] puzzles) 
    {
+       setNumPuzzlesToSolve();
        this.puzzles = puzzles;
        this.players = players;
+       done = false;
        
        beginGame();
    }
    
    public void beginGame()
    {
-       for(int i = 0; i < puzzles.length; i++)
+       for (int i = 0; i < numPuzzlesToSolve; i++)
        {
-           solveThePuzzle(puzzles[i]);
+           // TODO:  start with player 0, and continue until the puzzle is solved
        }
    }
    
@@ -33,5 +39,10 @@ public class Game
        {
            players[i].spinOrSolve(p);
        }
+   }
+   
+   public void setNumPuzzlesToSolve()
+   {
+       this.numPuzzlesToSolve = Tester.getUserInt("Enter the number of puzzles to solve: ");
    }
 }
