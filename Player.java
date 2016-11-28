3f9e7a4fae302a425e9bce60159ab6e7cd20ec45
wcam <campbell.programming@gmail.com>
2016-11-28 00:57:30 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -19,33 +19,70 @@ public class Player
    {
        boolean successfulGuess = false;
        
+       System.out.println("It's your turn " + getName() + "! Spin or solve? " + p.getHiddenPhrase());
+       System.out.println("Puzzle #" + p.getPuzzleNumber() + ", " + p.getCategory());
        int choice = Tester.getUserInt("(1) spin (2) solve");
        
-       int money = spinWheel();
-       System.out.println("The wheel landed on: " + money);
+       switch(choice)
+       {
+           case 1:
+               int money = spinWheel();
+               
+               if (money == 9999)
+               {
+                   System.out.println("You lost your turn! :(");
+               }
+               else if (money == 0000)
+               {
+                   System.out.println("You are BANKRUPT! Ouch! :(");
+                   setRoundBalance(0);
+               }
+               else
+               {
+                   System.out.println("The wheel landed on: " + money);
        
-       String guessInput = Tester.getUserInput("Enter a character: ");
-       char guessLetter = guessInput.charAt(0);
+                    String guessInput = Tester.getUserInput("Enter a character: ");
+                    char guessLetter = guessInput.charAt(0);
+
+                    System.out.println(getName() + " guessed " + guessLetter);
+
+                     int matches = 0;
+                     String puzzlePhrase = p.getHiddenPhrase();
+                     char[] thisPuzzle = puzzlePhrase.toCharArray();
+
+                     for (int k = 0; k < thisPuzzle.length; k++)
+                     {
+                         if (p.phrase[k] == guessLetter)
+                         {
+                             p.hiddenPhrase[k] = p.phrase[k];
+                             matches++;
+                             successfulGuess = true;
+                         }
+                     }
+                    setRoundBalance(getRoundBalance() + money * matches);
+                    System.out.println("Found " + matches + " matches!");
+                    System.out.println(p.getHiddenPhrase());
+               }
+                break;
+           case 2:
+               String guess = Tester.getUserInput("Try to solve!: ");
+               String theCorrectphrase = p.getPhrase();
+               
+               if (guess.equals(theCorrectphrase))
+               {
+                   successfulGuess = true;
+                   p.setSolved(true);
+                   setCashBalance(getCashBalance() + getRoundBalance());
+                   System.out.println("Success! Your total for the day is  $" + getCashBalance());
+               }
+               else
+               {
+                   System.out.println("fail.");
+                   successfulGuess = false;
+               }
+               break;
+       }
        
-       System.out.println("PUZZLE: " + p.getHiddenPhrase() + " and you guessed " + guessLetter);
-       
-        int matches = 0;
-        String puzzlePhrase = p.getHiddenPhrase();
-        char[] thisPuzzle = puzzlePhrase.toCharArray();
-        
-        for (int k = 0; k < thisPuzzle.length; k++)
-        {
-            if (p.phrase[k] == guessLetter)
-            {
-                p.hiddenPhrase[k] = p.phrase[k];
-                matches++;
-                roundBalance = roundBalance + money;
-                successfulGuess = true;
-            }
-        }
-        
-        System.out.println("Found " + matches + " matches! Round balance: " + roundBalance);
-        System.out.println(p.getHiddenPhrase());
         return successfulGuess;
    }
    
@@ -54,8 +91,75 @@ public class Player
    	this.name = name;
    }
    
+   public void setCashBalance(int cash)
+   {
+       this.cashBalance = cash;
+   }
+   
+   public int getCashBalance()
+   {
+       return cashBalance;
+   }
+   
    public int spinWheel()
    {
-       return Tester.randomNumber(0, 500);
+       int spin = Tester.randomNumber(0, 11);
+       int result = 0;
+       
+       switch(spin)
+       {
+           case 0:
+                result = 550;
+                break;
+           case 1:
+                result =  600;
+                break;
+           case 2:
+                result =  650;
+                break;
+           case 3:
+                result =  700;
+                break;
+           case 4:
+                result =  750;
+                break;
+           case 5:
+                result =  750;
+                break;
+           case 6:
+                result =  800;
+                break;
+           case 7:
+                result =  850;
+                break;
+           case 8:
+                result =  900;
+                break;
+           case 9:
+                result =  500;
+                break;
+           case 10:
+                result =  9999;
+                break;
+           case 11:
+                result =  0000;
+                break;
+       }
+       return result;
+   }
+   
+   public int getRoundBalance()
+   {
+       return roundBalance;
+   }
+   
+   public void setRoundBalance(int money)
+   {
+       roundBalance = money;
+   }
+   
+   public String getName()
+   {
+       return name;
    }
 }
